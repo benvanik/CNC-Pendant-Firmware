69f4b19b6a690740da8586f0233dad212185e000
Ben Vanik <ben.vanik@gmail.com>
2025-02-27 08:22:44 -0800

    RotaryEncoder update from forum thread. https://forum.duet3d.com/topic/36798/cnc-pendant-encoder-skipping-step Requires https://github.com/PaulStoffregen/Encoder.

--- src/CNC_pendant/RotaryEncoder.h
+++ src/CNC_pendant/RotaryEncoder.h
@@ -2,25 +2,20 @@
 #define __RotaryEncoderIncluded
 
 #include <Arduino.h>
+#include <Encoder.h>
 
 class RotaryEncoder
 {
-  uint8_t state;
-  int pin0, pin1;
-  int ppc;
-  int change;
+  Encoder enc;
 
-  uint8_t readState()
-  {
-    return (digitalRead(pin0) ? 1u : 0u) | (digitalRead(pin1) ? 2u : 0u);
-  }
+  int ppc;
+
+  int lastV;
 
 public:
   RotaryEncoder(int p0, int p1, int pulsesPerClick) : 
-    state(0), pin0(p0), pin1(p1), ppc(pulsesPerClick), change(0) {}
+    enc(p0, p1), ppc(pulsesPerClick), lastV(0) {}
 
-  void init();
-  void poll();
   int getChange();
 };
 
